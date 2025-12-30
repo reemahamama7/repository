2d22dbf9019cee072f1a8cb817342c2e18a4759f
remahamama7 <remarosa7@gmail.com>
2025-12-30 20:37:59 +0300

    feat: Add new API endpoint or fix bug

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,15 +5,14 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
+class ExampleTest extends TestCase {
     /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    * A basic test example.
+    */
 
-        $response->assertStatus(200);
+    public function test_the_application_returns_a_successful_response(): void {
+        $response = $this->get( '/' );
+
+        $response->assertStatus( 200 );
     }
 }
